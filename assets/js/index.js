c0735ce8073cac7dc1f1b0d14200a053f7de402b
Shibin Antony <shibincj@gmail.com>
2020-07-31 01:57:42 +0530

    v1

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+$('.quotes').owlCarousel({
+    items: 1,
+    margin: 10,
+    nav: true,
+    dots: false,
+})
+$( ".owl-prev").html('<i class="flaticon-left-arrow"></i>');
+$( ".owl-next").html('<i class="fa flaticon-next"></i>');
